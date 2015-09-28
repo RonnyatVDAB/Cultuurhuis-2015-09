3197156bcf3a59d54b7c7da547e77a829c11c139
Patrick Van den Bussche <pat.vdbussche@gmail.com>
2015-09-28 15:53:53 +0200

    voorstelling met genre manytoone

--- src/main/java/be/vdab/entities/Voorstelling.java
+++ src/main/java/be/vdab/entities/Voorstelling.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +19,9 @@ public class Voorstelling implements Serializable {
 	private long id;
 	private String titel;
 	private String uitvoerder;
-	private long genreid;
+	@ManyToOne(optional=false)
+	@JoinColumn(name='genreid')
+	private Genre genre;
 	
 	public String getVoorstelling(){
 		return titel + " " + uitvoerder;
