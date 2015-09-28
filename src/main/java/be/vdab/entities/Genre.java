6afc32a8b6845a061135c2d6632eda04961ebbd8
Steve Schoonooghe <Steve.Schoonooghe@gmail.com>
2015-09-28 14:15:07 +0200

    head tag, cultuurhuis.css, genre class, genredao(voorlopige)

---
+++ src/main/java/be/vdab/entities/Genre.java
@@ -0,0 +1,32 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "genres")
+public class Genre implements Serializable { 
+	
+	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue 
+	private long id;
+	private String naam; 
+	
+    public static long getSerialversionuid() {
+	return serialVersionUID;
+	}
+	
+    public long getId() {
+		return id;
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+}
