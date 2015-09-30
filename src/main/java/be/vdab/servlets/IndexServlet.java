84426aed4fde3cf5c4460bde4ecfa2009ab0ab08
guycastle <guillaume.vandecasteele@gmail.com>
2015-09-30 11:09:08 +0200

    Cleaned up extraneous files

--- src/main/java/be/vdab/servlets/IndexServlet.java
+++ src/main/java/be/vdab/servlets/IndexServlet.java
@@ -1,15 +1,14 @@
 package be.vdab.servlets;
 
-import java.io.IOException;
+import be.vdab.entities.Genre;
+import be.vdab.services.GenreService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import be.vdab.entities.Genre;
-import be.vdab.services.GenreService;
+import java.io.IOException;
 
 /**
  * Servlet implementation class IndexServlet
--- src/test/java/be/vdab/entities/GenreTest.java
+++ src/test/java/be/vdab/entities/GenreTest.java
@@ -1,6 +1,5 @@
 package be.vdab.entities;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GenreTest {
